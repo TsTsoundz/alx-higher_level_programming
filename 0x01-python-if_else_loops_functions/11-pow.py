3f272f6205b8524042867e1694bc4cb18e9ba41d
TsTsoundz <adacheprince@gmail.com>
2023-10-31 20:24:10 -0700

    My pyt3

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Prince Adache
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Prince Adache
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
